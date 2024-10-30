db022713cebc9b4780c42b9e7897a4a29fdbace1
nariwaa <lucasval3n2c@gmail.com>
2024-10-30 02:45:14 +0100

    did stuff (it doesn't work anymore)

---
+++ imports/laptop.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+{
+    [
+      ./modules/rofi.nix
+      ./modules/starship.nix
+      ./modules/waybar.nix
+      ./modules/default-apps.nix
+      ./modules/hyprland.nix
+      ./modules/fish.nix
+      ./modules/hyprlock.nix
+    ];
+}
