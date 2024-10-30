c2d15bb6186c54f0f4c0f7b1d9e7fbd315e8147e
nariwaa <lucasval3n2c@gmail.com>
2024-10-30 17:51:08 +0100

    fixed scroll and monitor issues

---
+++ modules/hyprland/input-gestures-desktop.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+
+      # Gesture settings
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_distance = 200;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_forever = true;
+        workspace_swipe_invert = true;
+      };
+
+      # Input settings
+      input = {
+        touchpad = {
+          natural_scroll = true;
+        };
+        scroll_factor = 1;
+        follow_mouse = 1;
+        kb_layout = "us";
+        kb_options = "$escswap, grp:alt_space_toggle";
+        kb_variant = "altgr-intl";
+        sensitivity = -0.5;
+      };
+    };
+  };
+}
---
+++ modules/hyprland/input-gestures-laptop.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+
+      # Gesture settings
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_distance = 200;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_forever = true;
+        workspace_swipe_invert = true;
+      };
+
+      # Input settings
+      input = {
+        touchpad = {
+          natural_scroll = true;
+        };
+        scroll_factor = 1;
+        scroll_method = 2fg;
+        follow_mouse = 1;
+        kb_layout = "us";
+        kb_options = "$escswap, grp:alt_space_toggle";
+        kb_variant = "altgr-intl";
+        sensitivity = 1;
+      };
+    };
+  };
+}
