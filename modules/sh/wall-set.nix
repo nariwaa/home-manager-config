5f2ed825509cadce26be68226a0f9da80750f892
nariwaa <nariwaa>
2024-12-14 16:21:59 +0100

    fixed wallpaper color not generating

--- modules/sh/wall-set.nix
+++ modules/sh/wall-set.nix
@@ -3,7 +3,7 @@
     text = ''
 
 cp $1 /tmp/current.png
-wal -i $1
+wal -i $1 --backend colorz 
 killall .waybar-wrapped
 setsid swww img $1 --resize crop -t random --transition-duration 3 --transition-fps 100 & disown
 sleep 3.5
--- modules/sh/wall-set.sh
+++ modules/sh/wall-set.sh
@@ -1,5 +1,5 @@
 cp $1 /tmp/current.png
-wal -i $1
+wal -i $1 --backend colorz 
 killall .waybar-wrapped
 setsid swww img $1 --resize crop -t random --transition-duration 3 --transition-fps 100 & disown
 sleep 3.5
