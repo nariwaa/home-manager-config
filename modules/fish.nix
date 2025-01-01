11765df2c4f41a3e379c7869a995aa1a2108d0a9
nariwaa <lucasval3n2c@gmail.com>
2025-01-01 21:21:46 +0100

    added an alias music-dlp to dl youtube video with exif data

--- modules/fish.nix
+++ modules/fish.nix
@@ -9,7 +9,10 @@ set -U fish_greeting
 cat /home/ayaya/.cache/wal/sequences  
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 eval $(keychain --eval --quiet --agents ssh banana)
+
+#alias
 alias e nvim
+alias music-dlp='yt-dlp -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata -o "%(creator,uploader,channel)s - %(title)s.%(ext)s"'
 
     '';
   };
