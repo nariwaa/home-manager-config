34b48da98acf223f97671e16ecb74193ac52f477
nariwaa <lucasval3n2c@gmail.com>
2025-01-15 20:16:19 +0100

    swtiched to brave for pdfs

--- modules/default-apps.nix
+++ modules/default-apps.nix
@@ -5,6 +5,7 @@
     defaultApplications = {
       # Web browser
       "text/html" = ["brave.desktop"];
+      "application/pdf" = ["brave.desktop"];
       "x-scheme-handler/http" = ["brave.desktop"];
       "x-scheme-handler/https" = ["brave.desktop"];
       "x-scheme-handler/about" = ["brave.desktop"];
