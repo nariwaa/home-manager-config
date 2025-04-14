f0e3cadf201903c7c14500bc5739f4653224b2c1
nariwaa <lucasval3n2c@gmail.com>
2025-04-14 15:54:57 +0200

    added lsd + auto tmux

--- modules/fish.nix
+++ modules/fish.nix
@@ -10,9 +10,16 @@ cat /home/ayaya/.cache/wal/sequences
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 eval (keychain --eval --agents ssh --quiet)
 
-#alias
+# cd stuff
+function cd
+    builtin cd $argv; and lsd
+end
+
+# alias
 alias e nvim
 alias music-dlp='yt-dlp -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata -o "%(creator,uploader,channel)s - %(title)s.%(ext)s"'
+
+tmux
 
     '';
   };
